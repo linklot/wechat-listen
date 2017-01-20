69a73fb7bfdd031e1cd2e9f9b76ecb429b692979
Lincoln Li <linklot@gmail.com>
2017-01-20 21:12:42 +1100

    External file test

--- src/main/java/com/lli/mp/Application.java
+++ src/main/java/com/lli/mp/Application.java
@@ -1,11 +1,15 @@
 package com.lli.mp;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.setLogStartupInfo(false);
+		app.run(args);
 	}
 }
